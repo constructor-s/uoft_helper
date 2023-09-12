d54c848d1214540b5efecf86b0cca9f152a842c5
Bill <10003477+constructor-s@users.noreply.github.com>
2023-09-11 20:35:53 -0400

    Check autofill color

--- uoft_helper.js
+++ uoft_helper.js
@@ -1,5 +1,5 @@
 /// uoft_login_helper.js
-function login(retry = true) {
+function login() {
     if (document.getElementsByClassName("form-error").length > 0) {
         console.log("There is error on the login page.");
     } else if (document.getElementById("username") && document.getElementById("username").value != "" && document.getElementById("password") && document.getElementById("password").value != "") {
@@ -7,11 +7,22 @@ function login(retry = true) {
         document.getElementsByClassName("btn-lg")[0].click();
     } else {
         console.log("Not logging in because username or password is empty.");
-        if (retry) {
-            console.log("Retrying in 1 second.");
-            setTimeout(() => document.getElementsByClassName("btn-lg")[0].click(), 1000);
-        }
+        loginIfAutocomplete();
+    }
+}
+function loginIfAutocomplete() {
+    // https://stackoverflow.com/questions/35049555/chrome-autofill-autocomplete-no-value-for-password
+    // Check if the background color of the username and password fields are white using getComputedStyle.
+    // If it is white, then the password field is not autofilled.
+    // If it is not white, then the password field is autofilled.
+    if (window.getComputedStyle(document.getElementById("username")).backgroundColor != "rgb(255, 255, 255)" && window.getComputedStyle(document.getElementById("password")).backgroundColor != "rgb(255, 255, 255)") {
+        console.log("Autofill detected. Logging in.");
+        login(false);
+    } else {
+        // Call this function later to check if the password field is autofilled.
+        console.log("Autofill not detected. Checking again in 1 second.");
+        setTimeout(loginIfAutocomplete, 1000);
     }
 }
 console.log("Injecting login to window.onload.")
-window.addEventListener("load", login, true);
+window.addEventListener("load", login);
